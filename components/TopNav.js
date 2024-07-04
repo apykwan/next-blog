ef70e8b8b660123ce0901a4b64060e62ee33900d
apykwan <apykwan@hotmail.com>
2024-07-03 19:32:35 -0700

    add search bar and search api

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,13 +1,31 @@
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
 
+import { useSearch } from '@/context/search';
+
 export default function TopNav() {
   const { data, status } = useSession();
   // console.log("data: ", data, "status: ", status);
 
+  const { searchQuery, setSearchQuery, fetchSearchResults } = useSearch();
+
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
       <Link className="nav-link" href="/">BLOG</Link>
+
+      <form className="d-flex" role="search" onSubmit={fetchSearchResults}>
+        <input 
+          type="search" 
+          className="form-control" 
+          placeholder="search" 
+          aria-label="search" 
+          onChange={e => setSearchQuery(e.target.value)}
+          value={searchQuery}
+        />
+        <button className="btn">
+          &#128270;
+        </button>
+      </form>
       {status === "loading" 
         ? null 
         : status === "authenticated" ? (
